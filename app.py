38ccf13aabeb430509810c98ac11cf7e29399996
SavviSam <swghazzawi@gmail.com>
2024-06-25 03:47:26 -0600

    Add error handling to callback function

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 import requests
 from requests.auth import HTTPBasicAuth
-from flask import Flask, request, redirect
+from flask import Flask, jsonify, request, redirect
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -21,22 +21,25 @@ def home():
 
 @app.route('/callback')
 def callback():
-    auth_code = request.args.get('code')
-    token_url = "https://oauth.platform.intuit.com/oauth2/v1/tokens/bearer"
-    auth = HTTPBasicAuth(client_id, client_secret)
-    headers = {"Accept": "application/json"}
-    data = {
-        "grant_type": "authorization_code",
-        "code": auth_code,
-        "redirect_uri": redirect_uri
-    }
+    try:
+      auth_code = request.args.get('code')
+      token_url = "https://oauth.platform.intuit.com/oauth2/v1/tokens/bearer"
+      auth = HTTPBasicAuth(client_id, client_secret)
+      headers = {"Accept": "application/json"}
+      data = {
+          "grant_type": "authorization_code",
+          "code": auth_code,
+          "redirect_uri": redirect_uri
+      }
 
-    response = requests.post(token_url, auth=auth, headers=headers, data=data)
-    tokens = response.json()
-    access_token = tokens["access_token"]
-    refresh_token = tokens["refresh_token"]
-    
-    return f'Access Token: {access_token}, Refresh Token: {refresh_token}'
+      response = requests.post(token_url, auth=auth, headers=headers, data=data)
+      tokens = response.json()
+      access_token = tokens["access_token"]
+      refresh_token = tokens["refresh_token"]
+      
+      return f'Access Token: {access_token}, Refresh Token: {refresh_token}'
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 # TODO: function for access and refresh tokens with auth code as input
 # TODO: function to refresh tokens once they expire (handle 401 status code error)
